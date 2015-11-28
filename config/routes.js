6df6092364ec8009d32a00c072b051aff2340942
Gev Gharadaghi <gowithgev@gmail.com>
2015-11-27 21:46:06 -0800

    heroku callback

--- config/routes.js
+++ config/routes.js
@@ -57,7 +57,7 @@ var stateKey = 'spotify_auth_state';
       response_type: 'code',
       client_id: process.env.CLIENT_ID,
       scope: scope,
-      redirect_uri: '/callback',
+      redirect_uri: 'https://peaceful-tor-6779.herokuapp.com/callback',
       state: state
     }));
 });
