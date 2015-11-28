f2425c17e4f00ef6b1e2d69c7cfb1353a4a9ccf0
Gev Gharadaghi <gowithgev@gmail.com>
2015-11-27 20:28:00 -0800

    callback set ups heroku

--- config/routes.js
+++ config/routes.js
@@ -57,7 +57,7 @@ var stateKey = 'spotify_auth_state';
       response_type: 'code',
       client_id: process.env.CLIENT_ID,
       scope: scope,
-      redirect_uri: 'http://localhost:3000/callback',
+      redirect_uri: 'http://localhost:3000/callback' || 'https://peaceful-tor-6779.herokuapp.com/callback',
       state: state
     }));
 });
